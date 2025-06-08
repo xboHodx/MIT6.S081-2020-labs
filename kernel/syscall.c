60669c8b462353e9f5257b7311a3f02d5c1f970d
Shenjie Yang <202430551774@mail.scut.edu.cn>
2025-06-08 12:42:19 +0800

    com

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -129,6 +129,7 @@ static uint64 (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_trace]   sys_trace,
+[SYS_sysinfo] sys_sysinfo,
 };
 
 static char *syscalls_name[] = {
@@ -154,6 +155,7 @@ static char *syscalls_name[] = {
 [SYS_mkdir]   "mkdir",
 [SYS_close]   "close",
 [SYS_trace]   "trace",
+[SYS_sysinfo] "sysinfo",
 };
 
 void
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -101,3 +101,21 @@ sys_trace(void){
   argint(0, &(myproc()->trace_mask));
   return 0;
 }
+
+uint64
+sys_sysinfo(void)
+{
+  struct sysinfo info;
+  freebytes(&info.freemem);
+  procnum(&info.nproc);
+
+  // get virtual address
+  uint64 dstaddr;
+  argaddr(0, &dstaddr);
+
+  // 从内核空间拷贝数据到用户空间
+  if (copyout(myproc()->pagetable, dstaddr, (char *)&info, sizeof info) < 0)
+    return -1;
+
+  return 0;
+}
