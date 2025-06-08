85fe4eecdfa09c66d9756a0f8aa02c1dc4370868
Shenjie Yang <202430551774@mail.scut.edu.cn>
2025-06-08 13:17:34 +0800

    c

--- kernel/defs.h
+++ kernel/defs.h
@@ -141,6 +141,9 @@ int             fetchstr(uint64, char*, int);
 int             fetchaddr(uint64, uint64*);
 void            syscall();
 
+// syscall.h
+struct          sysinfo;
+
 // trap.c
 extern uint     ticks;
 void            trapinit(void);
